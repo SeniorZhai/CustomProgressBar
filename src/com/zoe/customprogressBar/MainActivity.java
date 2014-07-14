dac57660ae27b6cbc7b393f9149bafedba5c5ce2
仔仔 <370985116@qq.com>
2014-07-14 13:47:44 +0800

    0714 CustomProgressBar

---
+++ src/com/zoe/customprogressBar/MainActivity.java
@@ -0,0 +1,12 @@
+package com.zoe.customprogressBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_main);
+	}
+}
